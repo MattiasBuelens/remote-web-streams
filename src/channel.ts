66b179410dacf7b87d6a5288e4f9eeb25108366a
Mattias Buelens <mattias@buelens.com>
2018-06-03 00:19:40 +0200

    No need to lazily initialize channel streams

--- src/channel.ts
+++ src/channel.ts
@@ -7,27 +7,15 @@ export class MessageChannelStream<T> implements ReadableWritableStreamPair<T, T>
 
   readonly readablePort: MessagePort;
   readonly writablePort: MessagePort;
-  _readable: ReadableStream<T> | undefined = undefined;
-  _writable: WritableStream<T> | undefined = undefined;
+  readonly readable: ReadableStream<T>;
+  readonly writable: WritableStream<T>;
 
   constructor() {
     const channel = new MessageChannel();
     this.readablePort = channel.port1;
     this.writablePort = channel.port2;
-  }
-
-  get readable() {
-    if (!this._readable) {
-      this._readable = fromReadablePort<T>(this.readablePort);
-    }
-    return this._readable;
-  }
-
-  get writable() {
-    if (!this._writable) {
-      this._writable = fromWritablePort<T>(this.writablePort);
-    }
-    return this._writable;
+    this.readable = fromReadablePort<T>(this.readablePort);
+    this.writable = fromWritablePort<T>(this.writablePort);
   }
 
 }
