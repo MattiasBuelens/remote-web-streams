c2b721584abf5278303532f78982b602cda7cd93
Mattias Buelens <mattias@buelens.com>
2018-06-12 23:37:17 +0200

    Properly mock MessagePort.start

--- test/mocks/MessageChannel.ts
+++ test/mocks/MessageChannel.ts
@@ -12,24 +12,65 @@ export class MockMessageChannel implements MessageChannel {
   }
 }
 
+export type MessageEventHandler = (this: MessagePort, ev: MessageEvent) => any;
+
 export class MockMessagePort extends EventTarget implements MessagePort {
-  public onmessage: ((ev: MessageEvent) => any) | null = null;
+  private _onmessage: MessageEventHandler | null = null;
   private _other: MockMessagePort | undefined = undefined;
+  private _queue: MessageEvent[] = [];
+  private _started: boolean = false;
 
   _entangle(other: MockMessagePort) {
     this._other = other;
   }
 
   start(): void {
+    if (this._started) {
+      return;
+    }
+    this._started = true;
+    const queue = this._queue.slice();
+    this._queue.length = 0;
+    for (let event of queue) {
+      this._dispatchMessageEvent(event);
+    }
   }
 
   postMessage(message?: any, transfer?: any[]): void {
     const event = new MessageEvent('message', { data: message });
-    this._other!.dispatchEvent(event); // TODO async
+    this._other!._postMessage(event);
   }
 
   close(): void {
     this._other = undefined;
+    this._queue.length = 0;
+  }
+
+  get onmessage(): MessageEventHandler | null {
+    return this._onmessage;
+  }
+
+  set onmessage(handler: MessageEventHandler | null) {
+    this._onmessage = handler;
+    // The first time a MessagePort object's onmessage IDL attribute is set,
+    // the port's port message queue must be enabled, as if the start() method had been called.
+    // https://html.spec.whatwg.org/multipage/web-messaging.html#message-ports:handler-messageport-onmessage-2
+    this.start();
+  }
+
+  private _postMessage(event: MessageEvent) {
+    if (this._started) {
+      this._dispatchMessageEvent(event);
+    } else {
+      this._queue.push(event);
+    }
+  }
+
+  private _dispatchMessageEvent(event: MessageEvent) {
+    this.dispatchEvent(event); // TODO async
+    if (typeof this._onmessage === 'function') {
+      this._onmessage(event);
+    }
   }
 
 }
