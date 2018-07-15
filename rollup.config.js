b4010b6b64093e2f8c353aec6ded446095fd084e
Mattias Buelens <mattias@buelens.com>
2018-07-16 00:09:25 +0200

    Output ES module

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,14 @@ const typescript = require('rollup-plugin-typescript2');
 
 module.exports = {
   input: './src/index.ts',
-  output: {
+  output: [{
     file: './dist/remote-web-streams.js',
     format: 'umd',
     name: 'RemoteWebStreams'
-  },
+  }, {
+    file: './dist/remote-web-streams.mjs',
+    format: 'es'
+  }],
   plugins: [
     typescript()
   ]
